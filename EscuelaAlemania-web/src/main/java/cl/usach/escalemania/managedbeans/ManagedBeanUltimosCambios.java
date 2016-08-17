6e2b006f469416934f1f42e169048243c864cf2e
gigoox <rodrigo.rivas@usach.cl>
2016-08-17 04:52:55 -0300

    Finalizadas HU-02

---
+++ EscuelaAlemania-web/src/main/java/cl/usach/escalemania/managedbeans/ManagedBeanUltimosCambios.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.usach.escalemania.managedbeans;
+
+import cl.usach.escalemania.entities.Documento;
+import cl.usach.escalemania.sessionbeans.AlertaFacadeLocal;
+import cl.usach.escalemania.sessionbeans.DocumentoFacadeLocal;
+import java.util.List;
+import java.util.Map;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+
+
+/**
+ *
+ * @author Rodrigo Rivas
+ */
+@ManagedBean
+@SessionScoped
+public class ManagedBeanUltimosCambios {
+
+    @EJB
+    private DocumentoFacadeLocal documentoFacade;
+    @EJB
+    private AlertaFacadeLocal alertaFacade;
+    
+    private List<Documento> documentos;
+    private List<Documento> documentosFiltrados;
+    private int  alertas;
+    private int alertasUsuario;
+    private int alertasTotal;
+    private String usuario;
+    private String rol;
+
+   
+    public void init(){
+        System.out.println("INIT");
+        FacesContext fc = FacesContext.getCurrentInstance();
+        Map<String, Object> sesisonMap = fc.getExternalContext().getSessionMap();
+        usuario = (String) sesisonMap.get("usuario");
+        rol = (String) sesisonMap.get("rol");
+        if (usuario==null) {
+            fc.getApplication().getNavigationHandler().handleNavigation(fc, null, "/home.xhtml?faces-redirect=true");
+        } else if (!FacesContext.getCurrentInstance().isPostback()) {
+            documentos = documentoFacade.findAll();
+            alertas = documentoFacade.obtenerAlertas(documentos);
+            alertasUsuario=alertaFacade.obtenerAlertas(usuario).size();
+            alertasTotal=alertas+alertasUsuario;
+            documentosFiltrados=null;
+        }
+    }
+
+    public List<Documento> getDocumentos() {
+        return documentos;
+    }
+
+    public void setDocumentos(List<Documento> documentos) {
+        this.documentos = documentos;
+    }
+
+    public List<Documento> getDocumentosFiltrados() {
+        return documentosFiltrados;
+    }
+
+    public void setDocumentosFiltrados(List<Documento> documentosFiltrados) {
+        this.documentosFiltrados = documentosFiltrados;
+    }
+
+    public int getAlertas() {
+        return alertas;
+    }
+
+    public void setAlertas(int alertas) {
+        this.alertas = alertas;
+    }
+
+    public int getAlertasUsuario() {
+        return alertasUsuario;
+    }
+
+    public void setAlertasUsuario(int alertasUsuario) {
+        this.alertasUsuario = alertasUsuario;
+    }
+
+    public int getAlertasTotal() {
+        return alertasTotal;
+    }
+
+    public void setAlertasTotal(int alertasTotal) {
+        this.alertasTotal = alertasTotal;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public ManagedBeanUltimosCambios() {
+    }
+    
+}
